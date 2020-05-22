f6a2ed1cec5ddbd64ad4a5f41f8f2955991f66c5
Akhil Reddy Mallidi <akhilreddy.mallidi@hcl.com>
2020-05-22 14:21:59 +0530

    Added Image links to the homepage component

--- src/components/MenuItem/MenuItem.component.jsx
+++ src/components/MenuItem/MenuItem.component.jsx
@@ -4,12 +4,13 @@ import './MenuItem.styles.css';
 
 const MenuItem = (props) => {
     return(
-        <div className = "MenuItem">
+        <div className = "MenuItem" style = {{ background : `url('https://i.ibb.co/cvpntL1/hats.png')` }}>
 
             <div className = "Content">
 
-                <h1 className = "Title"> First Item </h1>
+                <h1 className = "Title"> {props.title} </h1>
                 <span className = "Subtitle"> SHOP NOW </span>
+                <p> {props.imageUrl} </p>
 
             </div>
 
